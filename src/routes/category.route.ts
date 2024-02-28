23241fd6f00b74bb0db9703589834b2d0f5f67fb
Lalqh <lalincotero13@gmail.com>
2024-02-27 20:49:42 -0600

    Se agrego la autenticacion a las rutas de categorias y sucurales

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,37 +1,50 @@
 import express from 'express'
 import { Category } from '../types/category.type'
 import CategoryService from '../services/category.service'
+import passport from 'passport'
 
 const router = express.Router()
 const service = new CategoryService()
 
-router.post('/', async (req, res) => {
-  const category: Category = req.body
-  const newCategory = await service.create(category)
-  res.status(201).json(newCategory)
-})
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const category: Category = req.body
+    const newCategory = await service.create(category)
+    res.status(201).json(newCategory)
+  }
+)
 
-router.get('/', async (req, res, next) => {
-  try {
-    if (req.query.name) {
-      const category = await service.findByName(req.query.name as string)
-      res.status(200).json(category)
-    } else {
-      const categories = await service.findAll()
-      res.status(200).json(categories)
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      if (req.query.name) {
+        const category = await service.findByName(req.query.name as string)
+        res.status(200).json(category)
+      } else {
+        const categories = await service.findAll()
+        res.status(200).json(categories)
+      }
+    } catch (error) {
+      next(error)
     }
-  } catch (error) {
-    next(error)
   }
-})
+)
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const category = await service.findById(req.params.id)
-    res.status(200).json(category)
-  } catch (error) {
-    next(error)
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const category = await service.findById(req.params.id)
+      res.status(200).json(category)
+    } catch (error) {
+      next(error)
+    }
   }
-})
+)
 
 export default router
